db61c6f535265a05970c79071b02a1de87848283
vagrant <vagrant@rails-dev-box>
2014-01-07 06:12:55 +0000

    this is the initial state.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YellowPages::Application.config.secret_key_base = 'be164a99a10dc7bbef900fff777154eb15819c722fc301531effd1783ffc086f12241db1071a5c3c4d7e9c1f1999092b6338774d60a6964e86ffb3b8e238bae5'
